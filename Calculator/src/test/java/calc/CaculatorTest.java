83fd797b82fa2b19023d8eb0c94b5aca2749c278
RLX <ruanlx22@gmail.com>
2018-03-10 22:13:35 +0100

    set pom.xml

--- Calculator/src/test/java/calc/CaculatorTest.java
+++ Calculator/src/test/java/calc/CaculatorTest.java
@@ -18,23 +18,27 @@ public class CaculatorTest {
 		assertEquals(5, calc.add(3,2));
 	}
 	@Test
-	public void testSub() {
-		assertEquals(1, calc.sub(3,2));
+	public void testAdd1() {
+		assertEquals(1, calc.add(3,2));
 	}
-	@Test
-	public void testMul() {
-		assertEquals(6, calc.mul(3,2));
-	}
-	@Test
-	public void testDiv() {
-		assertEquals(1, calc.div(2,2));
-	}
-	@Test
-	public void testDivByZero() {
-		assertEquals(0, calc.div(3,0));
-	}
-	@Test
-	public void testPresedence() {
-		assertEquals(30, calc.add(calc.mul(5, 5), calc.div(10, 2)));
-	}
+//	@Test
+//	public void testSub() {
+//		assertEquals(1, calc.sub(3,2));
+//	}
+//	@Test
+//	public void testMul() {
+//		assertEquals(6, calc.mul(3,2));
+//	}
+//	@Test
+//	public void testDiv() {
+//		assertEquals(1, calc.div(2,2));
+//	}
+//	@Test
+//	public void testDivByZero() {
+//		assertEquals(0, calc.div(3,0));
+//	}
+//	@Test
+//	public void testPresedence() {
+//		assertEquals(30, calc.add(calc.mul(5, 5), calc.div(10, 2)));
+//	}
 }
