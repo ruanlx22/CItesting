be0b5a01feed50a17636f6a7468c7523f6a9ab23
nerozhang <neroztp@gmail.com>
2018-03-14 11:21:09 +0100

    add function mul

--- Calculator/src/main/java/calc/CalculatorMain.java
+++ Calculator/src/main/java/calc/CalculatorMain.java
@@ -7,7 +7,12 @@ public class CalculatorMain {
 	public int add(int a, int b){
 		return a+b;
 	}
+	
 	public int sub(int a, int b) {
 		return a-b;
+	}
+	
+	public int mul(int a, int b) {
+		return a*b;
 	}
 }
