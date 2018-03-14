940a73a26f8d83cf61af0ab6722205a2eef3cb6f
RLX <ruanlx22@gmail.com>
2018-03-14 11:19:34 +0100

    add div function in main

--- Calculator/src/main/java/calc/CalculatorMain.java
+++ Calculator/src/main/java/calc/CalculatorMain.java
@@ -7,4 +7,10 @@ public class CalculatorMain {
 	public int add(int a, int b){
 		return a+b;
 	}
+	public int div(int a, int b){
+		if(b==0) {
+			return 0;
+		}
+		return a/b;
+	}
 }
--- Calculator/src/test/java/calc/CaculatorTest.java
+++ Calculator/src/test/java/calc/CaculatorTest.java
@@ -21,24 +21,24 @@ public class CaculatorTest {
 	public void testAdd1() {
 		assertEquals(1, calc.add(0,1));
 	}
-//	@Test
-//	public void testSub() {
-//		assertEquals(1, calc.sub(3,2));
-//	}
-//	@Test
-//	public void testMul() {
-//		assertEquals(6, calc.mul(3,2));
-//	}
-//	@Test
-//	public void testDiv() {
-//		assertEquals(1, calc.div(2,2));
-//	}
-//	@Test
-//	public void testDivByZero() {
-//		assertEquals(0, calc.div(3,0));
-//	}
-//	@Test
-//	public void testPresedence() {
-//		assertEquals(30, calc.add(calc.mul(5, 5), calc.div(10, 2)));
-//	}
+	@Test
+	public void testSub() {
+		assertEquals(1, calc.sub(3,2));
+	}
+	@Test
+	public void testMul() {
+		assertEquals(6, calc.mul(3,2));
+	}
+	@Test
+	public void testDiv() {
+		assertEquals(1, calc.div(2,2));
+	}
+	@Test
+	public void testDivByZero() {
+		assertEquals(0, calc.div(3,0));
+	}
+	@Test
+	public void testPresedence() {
+		assertEquals(30, calc.add(calc.mul(5, 5), calc.div(10, 2)));
+	}
 }
